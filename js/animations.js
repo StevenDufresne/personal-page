920b85a59677370adc15e67a21f5e1dbfc89cc12
Steven Dufresne <dufresnesteven@gmail.com>
2016-09-18 13:05:16 -0400

    Update the inview logic to not care if its fully in view

--- js/animations.js
+++ js/animations.js
@@ -3,26 +3,25 @@ var Animator = {
 		var self = this;
 
 		$(window).scroll(function () {
-
 		   $("[data-animate-trigger]").each(function () {
 		      if (self.isInView(this) === true) {
 		      	var $this = $(this).find("[data-animate]")
 		          $this.addClass("animated " + $this.data("animate"))
 		      }
+
 		   });
-
-
 		});
 
 	},
 	isInView: function (elem) {
 	    var docViewTop = $(window).scrollTop();
 	    var docViewBottom = docViewTop + $(window).height();
+	    var padding = 100;
 
 	    var elemTop = $(elem).offset().top;
 	    var elemBottom = elemTop + $(elem).height();
 
-	    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));	
+	    return ((elemTop + padding <= docViewBottom) && (elemTop >= docViewTop));	
 	}
 }
 	
